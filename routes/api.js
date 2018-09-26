54d20a18eca001602216c1c9dccb538f7d48ba08
Harding <tdh@salina.k-state.edu>
2018-09-26 16:12:47 -0500

    add form to input data

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,6 @@ const Profile = require('../models/Profile')
 
 router.get('/profile', (req, res) => {
 	let filters = req.query
-	if (req.query.age != null){
-		filters = {
-			age: {$gt: req.query.age}
-		}
-	}
 
 	Profile.find(filters)
 	.then(profiles => {
@@ -86,9 +81,26 @@ router.get('/profile/:id', (req, res) => {
 })
 
 
-router.post('/profile', (req, res) => {
+router.post('/profile/input', (req, res) => {
 
 	Profile.create(req.body)
+	.then(profile => {
+		res.json({
+			confirmation: 'success',
+			data: profile
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+router.get('/profile/input', (req, res) => {
+
+	Profile.create(req.query)
 	.then(profile => {
 		res.json({
 			confirmation: 'success',
